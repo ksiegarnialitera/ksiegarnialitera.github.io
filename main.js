e7c08655f31c3794d01800069cbc82ca889d276c
Monika Dyrka <kasak.monika@wp.pl>
2020-09-03 17:13:02 +0200

    shadowing the boxes plus divs tooltip

--- main.js
+++ main.js
@@ -11,14 +11,79 @@ const misc1 = document.querySelector('.misc1');
 const textBox = document.querySelector('.text-box');
 const info = document.querySelector('.info');
 
-let boxesWithoutArticles = [musicNotes, books, misc2, watches, cds, postcards, coins, misc1, textBox, info];
+let boxesMatrixShadow = [articles, books, watches, coins, musicNotes, misc2, cds, postcards, misc1];
+let boxesAll = [articles, books, watches, coins, musicNotes, misc2, cds, postcards, misc1, info, textBox];
 
-articles.addEventListener('mouseover', function() {
-    musicNotes.classList.add('move-right');
-    boxesWithoutArticles.forEach(el => el.classList.add('shadow'));
-})
+for (let box of boxesMatrixShadow) {
+    box.addEventListener('mouseover', function() {
+        for (let i=0; i<boxesAll.length; i++) {
+            let boxes = boxesAll.filter(el => {return el !== box});
+            boxes[i].classList.add('shadow');
+        }
+    })
+    box.addEventListener('mouseout', function() {
+        for (let i=0; i<boxesAll.length; i++) {
+            let boxes = boxesAll.filter(el => {return el !== box});
+            boxes[i].classList.remove('shadow');
+        }
+    })
+}
 
-articles.addEventListener('mouseoout', function() {
-    musicNotes.classList.remove('move-right');
-    boxesWithoutArticles.forEach(el => el.classList.remove('shadow'));
-})
+// let boxesWithoutArticles = [musicNotes, books, misc2, watches, cds, postcards, coins, misc1, textBox, info];
+
+// articles.addEventListener('mouseover', function() {
+//     musicNotes.classList.add('move-down');
+//     cds.classList.add('move-down');
+//     postcards.classList.add('move-down');
+//     books.classList.add('move-down-right');
+//     misc1.classList.add('move-down-right');
+//     coins.classList.add('move-down-right');
+//     watches.classList.add('move-down-right');
+//     misc2.classList.add('move-down-right');
+//     info.classList.add('move-down-right');
+//     textBox.classList.add('move-down-right');
+//     boxesWithoutArticles.forEach(el => el.classList.add('shadow'));
+// })
+
+// articles.addEventListener('mouseout', function() {
+//     musicNotes.classList.remove('move-down');
+//     cds.classList.remove('move-down');
+//     postcards.classList.remove('move-down');
+//     books.classList.remove('move-down-right');
+//     misc1.classList.remove('move-down-right');
+//     coins.classList.remove('move-down-right');
+//     watches.classList.remove('move-down-right');
+//     misc2.classList.remove('move-down-right');
+//     info.classList.remove('move-down-right');
+//     textBox.classList.remove('move-down-right');
+//     boxesWithoutArticles.forEach(el => el.classList.remove('shadow'));
+// })
+
+
+
+
+// let boxesMatrix = [[articles, books, watches, coins], [musicNotes, books, misc2, info], [cds, books, textBox, textBox], [postcards, misc1, textBox, textBox]];
+
+
+// for (let i=0; i<4; i++) {
+//     for (let j=0; j<4; j++) {
+//         // console.log(boxesMatrix[i][j]);
+//         // boxesMatrix[i][j].addEventListener('mouseover', function() {
+//         //      let boxesWithoutTheBox = (box) => {
+                
+//         //     )}
+//             // let boxes = boxesMatrix[i][j].filter(
+//             //     box => {return boxesMatrix[i][j] !== boxesMatrix[0][1]});
+            
+//             boxesMatrix[i][j].addEventListener('mouseover', function() {
+//                 // function isBox(elements) {
+//                 //     return boxesMatrix[i][j] !== elements;
+//                 // }
+//                 // console.log(boxesMatrix.filter(isBox));
+//                 boxesMatrix.forEach(el => console.log(...el));
+//                     //.classList.add('shadow')
+//             })
+//         }
+//     }
+
+    //el.classList.add('shadow')
