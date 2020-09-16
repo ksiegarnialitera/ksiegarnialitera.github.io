94389985496c30109944206332d4239e311d3ed4
Monika Dyrka <kasak.monika@wp.pl>
2020-09-16 16:24:52 +0200

    magnifier and angle down functionalities

--- main.js
+++ main.js
@@ -112,3 +112,13 @@ for (let i=0; i<images.length; i++) {
 
     //el.classList.add('shadow')
 
+/*Resizing the image in the box*/
+
+const feedBox = document.querySelectorAll('.list-box');
+const feedImage = document.querySelectorAll('.boxImage');
+
+
+for(let i=0; i<feedImage.length ; i++) {
+    let imageWidth = feedImage[i].clientWidth;
+    feedBox[i].style.width = (imageWidth + 3) + 'px';
+};
