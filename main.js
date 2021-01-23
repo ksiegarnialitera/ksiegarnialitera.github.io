e3422a1c700e3431a3a20ed0163ee79e3e4afe8d
Monika Dyrka <dyrka.monika@gmail.com>
2021-01-23 20:36:28 +0100

    index changes

--- main.js
+++ main.js
@@ -29,96 +29,17 @@ for (let box of boxesMatrixShadow) {
     })
 }
 
-const arrowLeft = document.querySelector('.angle-left');
-const arrowRight = document.querySelector('.angle-right');
-const pictureFirst = document.querySelector('.pic1');
-const pictureSecond = document.querySelector('.pic2');
-const pictureThird = document.querySelector('.pic3');
-const carrousel = document.querySelector('.booksTop');
+let boxes = document.querySelectorAll('.boxes');
+let boxImages = document.querySelectorAll('.box-images');
 
-books.addEventListener('mouseover', function() {
-    carrousel.classList.remove('invisibilityTop');
-    carrousel.classList.add('animation');
-})
+for (let box of boxes) {
+    box.addEventListener('mouseover', function() {
+        boxImages.classList.remove('invisibilityTop');
+        boxImages.classList.add('animation');
+    })
 
-books.addEventListener('mouseout', function() {
-    carrousel.classList.add('invisibilityTop');
-    carrousel.classList.remove('animation');
-})
-
-let images =[pictureSecond, pictureThird, pictureFirst];
-
-arrowRight.addEventListener('onClick', function() {
-for (let i=0; i<images.length; i++) {
-    
+    box.addEventListener('mouseout', function() {
+        boxImages.classList.add('invisibilityTop');
+        boxImages.classList.remove('animation');
+    })
 }
-})
-// let boxesWithoutArticles = [musicNotes, books, misc2, watches, cds, postcards, coins, misc1, textBox, info];
-
-// articles.addEventListener('mouseover', function() {
-//     musicNotes.classList.add('move-down');
-//     cds.classList.add('move-down');
-//     postcards.classList.add('move-down');
-//     books.classList.add('move-down-right');
-//     misc1.classList.add('move-down-right');
-//     coins.classList.add('move-down-right');
-//     watches.classList.add('move-down-right');
-//     misc2.classList.add('move-down-right');
-//     info.classList.add('move-down-right');
-//     textBox.classList.add('move-down-right');
-//     boxesWithoutArticles.forEach(el => el.classList.add('shadow'));
-// })
-
-// articles.addEventListener('mouseout', function() {
-//     musicNotes.classList.remove('move-down');
-//     cds.classList.remove('move-down');
-//     postcards.classList.remove('move-down');
-//     books.classList.remove('move-down-right');
-//     misc1.classList.remove('move-down-right');
-//     coins.classList.remove('move-down-right');
-//     watches.classList.remove('move-down-right');
-//     misc2.classList.remove('move-down-right');
-//     info.classList.remove('move-down-right');
-//     textBox.classList.remove('move-down-right');
-//     boxesWithoutArticles.forEach(el => el.classList.remove('shadow'));
-// })
-
-
-
-
-// let boxesMatrix = [[articles, books, watches, coins], [musicNotes, books, misc2, info], [cds, books, textBox, textBox], [postcards, misc1, textBox, textBox]];
-
-
-// for (let i=0; i<4; i++) {
-//     for (let j=0; j<4; j++) {
-//         // console.log(boxesMatrix[i][j]);
-//         // boxesMatrix[i][j].addEventListener('mouseover', function() {
-//         //      let boxesWithoutTheBox = (box) => {
-                
-//         //     )}
-//             // let boxes = boxesMatrix[i][j].filter(
-//             //     box => {return boxesMatrix[i][j] !== boxesMatrix[0][1]});
-            
-//             boxesMatrix[i][j].addEventListener('mouseover', function() {
-//                 // function isBox(elements) {
-//                 //     return boxesMatrix[i][j] !== elements;
-//                 // }
-//                 // console.log(boxesMatrix.filter(isBox));
-//                 boxesMatrix.forEach(el => console.log(...el));
-//                     //.classList.add('shadow')
-//             })
-//         }
-//     }
-
-    //el.classList.add('shadow')
-
-/*Resizing the image in the box*/
-
-const feedBox = document.querySelectorAll('.list-box');
-const feedImage = document.querySelectorAll('.boxImage');
-
-
-for(let i=0; i<feedImage.length ; i++) {
-    let imageWidth = feedImage[i].clientWidth;
-    feedBox[i].style.width = (imageWidth + 3) + 'px';
-};
